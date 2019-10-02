8c2e93f6440c0b4813e512209a425787b2aab4cf
sudhigk <gksudhee6@gmail.com>
2019-10-03 02:51:41 +0530

    convolution in simple matrix

--- convolution.py
+++ convolution.py
@@ -1,24 +1,26 @@
 import cv2
 import numpy as np
-# img1 = cv2.imread('images/test.jpg',cv2.IMREAD_GRAYSCALE)
-# (d1,d2) = img1.shape
-# print(d1,d2)
+img1 = cv2.imread('images/test.jpg',cv2.IMREAD_GRAYSCALE)
+(d1,d2) = img1.shape
+print(d1,d2)
 
-img1 = np.zeros((2,3,3), np.uint8)
-d1 = 2
-d2 = 3
-nm = 4
-for i in range(0,2):
-    for j in range(0,3):
-        img1[i,j,0] = nm
-        img1[i, j, 1] = nm
-        img1[i, j, 2] = nm
-        nm = nm +1
+# img1 = np.zeros((2,3,3), np.uint8)
+# d1 = 2
+# d2 = 3
+# nm = 4
+# for i in range(0,2):
+#     for j in range(0,3):
+#         img1[i,j,0] = nm
+#         img1[i, j, 1] = nm
+#         img1[i, j, 2] = nm
+#         nm = nm +1
 
+md1 = 3
+md2 = 1
 
-mask = np.zeros((3,1,3), np.uint8)
-for i in range(0,3):
-    for j in range(0,1):
+mask = np.zeros((md1,md2,3), np.uint8)
+for i in range(0,md1):
+    for j in range(0,md2):
         mask[i,j] = 1
 
 nd1 = d1+3-1
@@ -42,15 +44,20 @@ cv2.moveWindow(w2,0,400)
 for i in range(0,nd1):
     for j in range(0,nd2):
         s = 0
-        for m in range(0,3):
+        for m in range(0,md1):
             if((i-m)>=0 and (i-m)<d1):
-                for n in range(0,1):
+                for n in range(0,md2):
                     if((j-n)>=0 and (j-n)<d2):
                         # print(m, n)
                         s = s + mask[m, n] * img1[i-m, j-n]
         otpt[i,j] = s
 
-for i in range(0,nd1):
-    print("\n")
-    for j in range(0,nd2):
-        print(otpt[i,j,0]," ")
+# for i in range(0,nd1):
+#     print("\n")
+#     for j in range(0,nd2):
+#         print(otpt[i,j,0]," ")
+
+cv2.imshow(w1,img1)
+cv2.imshow(w2,otpt)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
