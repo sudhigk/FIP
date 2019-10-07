dab1017312660ef349ea94acc8f0f002fde04e88
sudhigk <gksudhee6@gmail.com>
2019-10-07 23:08:03 +0530

    convolution in image using mask

--- convolution.py
+++ convolution.py
@@ -17,12 +17,13 @@ print(d1,d2)
 #         nm = nm +1
 
 md1 = 3
-md2 = 1
+md2 = 3
 
-mask = np.zeros((md1,md2,3), np.uint8)
+# mask = np.zeros((md1,md2,3), np.uint8)
+mask =np.array([[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]])
 for i in range(0,md1):
     for j in range(0,md2):
-        mask[i,j] = 1
+        mask[i,j] = 1/9
 
 nd1 = d1+3-1
 nd2 = d2+1-1
@@ -42,16 +43,20 @@ cv2.namedWindow(w2,cv2.WINDOW_FREERATIO)
 cv2.resizeWindow(w2,400,400)
 cv2.moveWindow(w2,0,400)
 
+for i in range(0,md1):
+    for j in range(0,md2):
+        print(mask[i,j])
+
 for i in range(0,nd1):
     for j in range(0,nd2):
-        s = 0
+        s = 0.0
         for m in range(0,md1):
             if((i-m)>=0 and (i-m)<d1):
                 for n in range(0,md2):
                     if((j-n)>=0 and (j-n)<d2):
                         # print(m, n)
-                        s = s + mask[m, n] * img1[i-m, j-n]
-        otpt[i,j] = s
+                        s = float(s) + float(mask[m, n]) * float(img1[i-m, j-n])
+        otpt[i,j] = int(s)
 
 # for i in range(0,nd1):
 #     print("\n")
