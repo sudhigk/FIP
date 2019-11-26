44af5278f74c90d5c410b9df2372589313ff073c
sudhigk <gksudhee6@gmail.com>
2019-11-26 10:43:35 +0530

    lab exam

--- labexam.py
+++ labexam.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('images/test.png',cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('images/test.jpg',cv2.IMREAD_GRAYSCALE)
 
 
 (d1,d2)=img.shape
@@ -15,14 +15,17 @@ l=-1
 for i in range(0,d1):
     for j in range(0,d2):
         temp = img[i,j]
-        r[temp]=1
+        r[temp]=r[temp]+1
 
 for i in range(0,256):
-    if(r[i]==1):
+    if(r[i]!=0):
         s=l
         l=i
 
+# for i in range(0,256):
+#     print(i,r[i])
+
 if(s!=-1):
-    print('second largest value :',s)
+    print('second largest intensity :',s,'\tcount:',r[s])
 else:
-    print('image has a single intensity value ',l)
+    print('image has a single intensity value ',l,'count:',l)
